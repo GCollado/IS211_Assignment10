95fc61c83f57ee8185ca2ecff4e05b542fd50f25
GCollado <gabriel.collado@spsmail.cuny.edu>
2020-12-15 01:25:36 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -5,5 +5,4 @@
 '''artist = cursor.execute(INSERT INTO artists(artist_name) VALUES(?)''', (artist_name)')''
 '''album, = cursor.execute(INSERT INTO albums(artist_name) VALUES(?)''', (artist_name)')''
 '''cursor.execute(INSERT INTO songs(artist_name) VALUES(?)''', (artist_name)')''
-
 """
